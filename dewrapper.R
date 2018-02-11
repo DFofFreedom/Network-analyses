d332d4eb6d597c03c99e17ce62957bc34eccf189
jevansbio <32939196+jevansbio@users.noreply.github.com>
2018-02-11 17:38:07 -0500

    Update dewrapper
    Original was not the latest version 
    Modified to check if data exists and then choose a subset to read in.

--- dewrapper.R
+++ dewrapper.R
@@ -1,4 +1,4 @@
-setwd("C:/Users/jevan8/Dropbox/POSTDOC/Network_generator")
+setwd("C:/Users/Julian/Dropbox/POSTDOC/Network_generator")
 exportdir='test'
 ptoread=dir(exportdir)
 plist=strsplit(ptoread,"_")
@@ -6,10 +6,28 @@ ddf=do.call(rbind,lapply(plist,FUN=as.numeric))
 colnames(ddf)=c("d.eff","i.dens","o.dens","m.i.eff","sex.eff","obs.eff","timesteps")
 ddf=data.frame(filename=ptoread,ddf)
 
-
-datalist=as.list(rep(NA,nrow(ddf)))
-
+#check if these combos have data
+ddf$DATA=F
 for(filename in ptoread){
+	typestoread=dir(file.path(exportdir,filename))
+	typelist=as.list(rep(NA,6))
+	names(typelist)=typestoread
+	for(type in typestoread){
+		repstoread=dir(file.path(exportdir,filename,type))
+		if(length(repstoread)>0){
+			ddf$DATA[ddf$filename==filename]=T
+		}
+	}
+	
+
+}
+
+#example of selecting and reading in data
+ddf2=ddf[ddf$d.eff==4&ddf$DATA==T,]
+
+#Read data into this dataframe.
+ddf2$data=I(as.list(rep(NA,nrow(ddf2))))
+for(filename in ddf2$filename){
 	typestoread=dir(file.path(exportdir,filename))
 	typelist=as.list(rep(NA,6))
 	names(typelist)=typestoread
@@ -22,7 +40,7 @@ for(filename in ptoread){
 		typelist[[which(typestoread==type)]]=repslist
 	}
 	
-	datalist[[which(ddf$filename==filename)]]=typelist
+	ddf2$data[[which(ddf2$filename==filename)]]=typelist
 }
 
 
